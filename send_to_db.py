477cdd152d6d7db87ffe50080e6aa6f28e6bef5e
Alex Beltran <alexbeltran@berkeley.edu>
2023-05-12 00:40:31 -0700

    attempt float conversion

--- send_to_db.py
+++ send_to_db.py
@@ -11,15 +11,26 @@ collec = os.environ.get('COLLECTION')
 
 if all([user, password, host, port]):
     if database and collec:
-        client = pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}') 
+        client = pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}')
         db = getattr(client, database)
         collection = getattr(db, collec)
         record = {}
         for key, value in os.environ.items():
             if key.startswith('SUBALIGNER_'):
                 clean_key = key.replace('SUBALIGNER_', '')
+                try:
+                    value = float(value)
+                except ValueError:
+                    pass 
                 record[clean_key] = value
         collection.insert_one(record)
+
+        collection = getattr(db, collec + '_short')
+        record_short = {}
+        for key, value in record:
+            if 'path' not in key and 'ANALYTICS_MONGODB' not in key:
+                record_short[key] = value
+        collection.insert_one(record_short)
     else:
         raise Exception('Must specify mongo db and collection names!')
 else:
