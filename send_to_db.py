7fc0d81e578b1d7c6ef01da9c8f38ecabfb7aa15
Alex Beltran <alexbeltran@berkeley.edu>
2023-05-11 17:57:34 -0700

    fix syntax error near client def

--- send_to_db.py
+++ send_to_db.py
@@ -10,15 +10,14 @@ database = os.environ.get('DB')
 collec = os.environ.get('COLLECTION')
 
 if all([user, password, host, port]):
-    client = 
-pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}')
+    client = pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}')
     if database and collec:
-        db = getattr(client)
+        db = getattr(client, database)
         collection = getattr(db, collec)
         record = {}
         for key, value in os.environ.items():
             if key.startswith('SUBALIGNER_'):
-                clean_key = key.copy().replace('SUBALIGNER_', '')
+                clean_key = key.replace('SUBALIGNER_', '')
                 record[clean_key] = value
         collection.insert_one(record)
     else:
